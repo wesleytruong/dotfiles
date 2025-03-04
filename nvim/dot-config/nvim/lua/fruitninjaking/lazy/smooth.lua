e2528e0437191d15f9326fc93d1ecd8e0fa451d5
wesleytruong <91992558+wesleytruong@users.noreply.github.com>
2025-03-03 21:00:43 -0800

    updated smooth

--- nvim/dot-config/nvim/lua/fruitninjaking/lazy/smooth.lua
+++ nvim/dot-config/nvim/lua/fruitninjaking/lazy/smooth.lua
@@ -4,14 +4,14 @@ return {
     opts = {
       -- smear_between_neighbor_lines = false,
       stiffness = 1,
-      trailing_stiffness = 0.3,
+      trailing_stiffness = 0.5,
       legacy_computing_symbols_support = true, -- removes shadow for transparent bgs
     },
   },
-  {
-    "karb94/neoscroll.nvim",
-    opts = {
-      duration_multiplier = 0.3,
-    },
-  }
+  -- {
+  --   "karb94/neoscroll.nvim",
+  --   opts = {
+  --     duration_multiplier = 0.3,
+  --   },
+  -- }
 }
