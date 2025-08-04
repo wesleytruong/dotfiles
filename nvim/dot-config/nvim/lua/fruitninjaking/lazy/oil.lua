99a666e12fb3fdf3944af215e3fce7f6c8db8424
wesleytruong <91992558+wesleytruong@users.noreply.github.com>
2025-08-03 22:53:41 -0700

    update oil

--- nvim/dot-config/nvim/lua/fruitninjaking/lazy/oil.lua
+++ nvim/dot-config/nvim/lua/fruitninjaking/lazy/oil.lua
@@ -6,8 +6,6 @@ return {
     keymaps = {
     ["g?"] = { "actions.show_help", mode = "n" },
     ["<CR>"] = "actions.select",
-    -- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-    -- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
     ["<C-t>"] = { "actions.select", opts = { tab = true } },
     ["<C-p>"] = "actions.preview",
     ["<C-c>"] = { "actions.close", mode = "n" },
@@ -18,7 +16,7 @@ return {
     ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
     ["gs"] = { "actions.change_sort", mode = "n" },
     ["gx"] = "actions.open_external",
-    ["g."] = { "actions.toggle_hidden", mode = "n" },
+    ["<leader>."] = { "actions.toggle_hidden", mode = "n" },
     ["g\\"] = { "actions.toggle_trash", mode = "n" },
   },
   -- Set to false to disable all of the above keymaps
