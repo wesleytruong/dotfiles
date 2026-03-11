5bc366d4368c7fcf735b4db64c2151354fcbc356
Wesley Truong <wesleytruong@ucsb.edu>
2026-03-11 03:30:25 -0700

    suppress transparent bg error in nvim notify

--- nvim/dot-config/nvim/lua/fruitninjaking/lazy/noice.lua
+++ nvim/dot-config/nvim/lua/fruitninjaking/lazy/noice.lua
@@ -1,15 +1,26 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
+  {
     "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#1a1b26",
+      max_height = 2,
+      render = "minimal",
+      stages = "static",
     }
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 }
